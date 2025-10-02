5ca468c9eed4583af255e6a014211fb3eb614fc5
Kirill4ch <kirillbakutin23@gmail.com>
2025-10-02 23:46:32 +0300

    комментарий

---
+++ pr7num1.py
@@ -0,0 +1,33 @@
+
+import math
+
+def rect(a, b):
+    return a * b;
+
+def triangle(a, b, c):
+    return (a+b+c)/2;
+
+def circle(diam):
+    return f"{((diam**2 * math.pi)):.4f}" #подсмотрел как через f-строку ограничить количество символов после точки
+
+
+print("Выберете фигуру: \n1.Квадрат \n2.Треугольник \n3.Круг")
+select = int(input())
+if select == 1:
+    print("Введите первую сторону")
+    a = int(input())
+    print("Введите вторую сторону")
+    b = int(input())
+    print("Площадь квадрата = " + str(rect(a, b)))
+if select == 2:
+    print("Введите первую сторону")
+    a = int(input())
+    print("Введите вторую сторону")
+    b = int(input())
+    print("Введите третью сторону")
+    c = int(input())
+    print("Площадь треугольника = " + str(rect(a, b, c)))
+if select == 3:
+    print("Введите диаметр круга")
+    diam = int(input())
+    print("Площадь круга = "+str(circle(diam)))
