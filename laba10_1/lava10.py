9ac7936808b898e7b2450be23d0b0e6e86664421
Kirill4ch <kirillbakutin23@gmail.com>
2025-11-23 18:47:10 +0300

    Add files via upload

---
+++ laba10_1/lava10.py
@@ -0,0 +1,29 @@
+
+def matrica(file):
+    matr = []
+    with open(file, "r") as f:
+        for line in f:
+            row = [int(x) for x in line.split()]
+            matr.append(row)
+    return matr
+
+
+def vivod(matr, name):
+    f = open(name, "w")
+    for line in matr:
+        for w in line: 
+            f.write(str(w) + " ")
+        f.write('\n')
+
+
+        
+
+matrc = matrica('vvod.txt')
+def sumSD(matr):
+    sum = 0
+    for i in range(len(matr) - 1):
+        sum += matr[i][i + 1]
+        file = open('vivod.txt', 'w')
+        file.write(str(sum))
+
+sumSD(matrc)
